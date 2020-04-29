f489a0d4164e2eb84179b68034feda11791ea2c6
deadcell <brianbb174@protonmail.com>
2020-04-29 09:39:32 -0700

    remove opp

--- pclass.py
+++ pclass.py
@@ -1,8 +1,10 @@
 class User:
-    def __init__(self, first, last):
+    def __init__(self, first, last, age):
         self.first = first,
         self.last = last,
+        self.age = age
 
 
-user1 = User("joe","mcman")
-print(user1)
+user1 = User("joe","mcman", 55)
+user1 = User("annaelle","nubs", 34)
+print(user1.first, user1.last, user1.age)
