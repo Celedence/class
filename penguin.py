0f34b2dc093e009c1410decb1681fb292897b9ec
deadcell <brianbb174@protonmail.com>
2020-04-29 18:22:46 -0700

    add pengiun

---
+++ penguin.py
@@ -0,0 +1,17 @@
+
+
+class Abilitiy:
+    def run(self):
+        return "run"
+
+    def walk(self):
+        return "walk"
+
+    def slide(self):
+        return "slide"
+
+
+penguin = Abilitiy()
+
+print(penguin.run())
+print(penguin.walk())
