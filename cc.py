035eacddb41e0d80cf0b289daaacd753787a4745
deadcell <brianbb174@protonmail.com>
2020-04-30 13:41:05 -0700

    add new method

--- cc.py
+++ cc.py
@@ -9,3 +9,10 @@ class Chicken:
         self.eggs += 1
         Chicken.total_eggs +=1
         return self.eggs
+
+
+c1 = Chicken("henrietta","white")
+c2 = Chicken("clucky","not so white")
+print(c1.lay_eggs())
+print(c2.lay_eggs())
+print(Chicken.total_eggs)
