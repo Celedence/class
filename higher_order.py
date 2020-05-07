507143585603330cf0f0300d930ff335996a6ad7
deadcell <brianbb174@protonmail.com>
2020-05-07 09:07:54 -0700

    add another higher order

--- higher_order.py
+++ higher_order.py
@@ -27,3 +27,16 @@ def greet(person):
     return result
 
 print(greet("Toby"))
+
+
+# another way of writing a higher order
+
+def make_laugh_at_func(person):
+    def get_laugh():
+        laugh = choice(("hahah","heheh","lol"))
+        return f"{laugh} {person}"
+    return get_laugh
+
+laugh_at = make_laugh_at_func("linda")
+
+print(laugh_at())
