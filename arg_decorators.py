864478b9a14e73e04c5dbb5176ae8764fee41e56
deadcell <brianbb174@protonmail.com>
2020-05-07 11:08:27 -0700

    add dunder method

--- arg_decorators.py
+++ arg_decorators.py
@@ -19,3 +19,18 @@ print(greet("brian"))
 print(order("burger","fries"))
 
 print(lol())
+
+#==========
+def log_function_data(fn):
+    def wrapper(*args, **kwargs):
+        print(f"you are calling the {fn.__name__} function")
+        print(f"Here's the documentation: {fn.__doc__}")
+        return fn(*args, **kwargs)
+    return wrapper
+
+@log_function_data
+def add(x, y):
+    """Adds 2 numbers together"""
+    return x + y
+
+print(add(23, 23))
