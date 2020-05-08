3a88a3db3372735ce8753183f7ff7251bd044d6a
deadcell <brianbb174@protonmail.com>
2020-05-08 13:55:32 -0700

    add new method

--- examples_decorators.py
+++ examples_decorators.py
@@ -12,3 +12,12 @@ def add(x, y):
     return(x + y)
 
 print(add(2,3))
+# ===================
+
+def ensure_fewer_than_three(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if args > 3
+            raise ValueError ("must be 3 or less")
+        return fn(*args, **kwargs)
+    return wrapper
