db319b7e65077a163129802dca51ed0d863d1641
deadcell <brianbb174@protonmail.com>
2020-05-08 14:13:21 -0700

    fixed typos

--- examples_decorators.py
+++ examples_decorators.py
@@ -17,7 +17,28 @@ print(add(2,3))
 def ensure_fewer_than_three(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        if args > 3
+        if args < 3:
             raise ValueError ("must be 3 or less")
         return fn(*args, **kwargs)
     return wrapper
+
+    # ===========================
+
+def only_ints(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if any([arg for arg in args if type(arg) !=int]):
+            return "Please only enter integers"
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+@only_ints
+def multi(x, y):
+    return x *y
+
+print(multi(2,3))
+
+
+
+
