18fb5c8202e7bd6568fda8b3839e6c2480690d38
deadcell <brianbb174@protonmail.com>
2020-05-13 10:58:50 -0700

    changed order of arguments

--- ctest.py
+++ ctest.py
@@ -1,11 +1,18 @@
-import unittest
+
 from card import Card
+from card import Deck
+import unittest
 
 class CardTest(unittest.TestCase):
     def setUp(self):
-        self.card = Card("Hearts", "A")
+        self.card = Card("A", "Hearts")
 
     def test_init(self):
         """cards should have a suit and value"""
         self.assertEqual(self.card.suit, "Hearts")
         self.assertEqual(self.card.value, "A")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
