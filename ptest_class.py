b1795633db2dcf34d9610b7bd684fd77706c5fa5
deadcell <brianbb174@protonmail.com>
2020-05-13 10:26:26 -0700

    add set up

--- ptest_class.py
+++ ptest_class.py
@@ -2,15 +2,16 @@ import unittest
 from game_demo import Prot
 
 class ProtTests(unittest.TestCase):
+    def setUp(self):
+        self.pickles = Prot("Pickles", exp= 50)
+
     def test_earned(self):
-        pickles = Prot("Pickles", exp= 50)
-        pickles.earned()
-        self.assertEqual(pickles.exp, 100)
+        self.pickles.earned()
+        self.assertEqual(self.pickles.exp, 100)
 
     def test_say_name(self):
-        pickles = Prot("Pickles", exp= 50)
-        pickles.say_name()
-        self.assertEqual(pickles.name, "Pickles")
+        self.pickles.say_name()
+        self.assertEqual(self.pickles.name, "Pickles")
         
 
 if __name__ == "__main__":
